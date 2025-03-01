426aecef23ff63038687b1d8529efd37105f1325
Dominiq-De-Coco <noahguilleminot@gmail.com>
2025-03-01 02:11:25 -0800

    Ajout des fonctions s_abs et s_pow dans s_math.s

--- makefile
+++ makefile
@@ -5,11 +5,14 @@ LD=ld
 
 all: libs_libc.so
 
-libs_libc.so: src/s_string.o
-	$(CC) -shared -o lib/libs_libc.so src/s_string.o
+libs_libc.so: src/s_string.o src/s_math.o
+	$(CC) -shared -o lib/libs_libc.so src/s_string.o src/s_math.o
 
 src/s_string.o: src/s_string.s
 	$(AS) -o src/s_string.o src/s_string.s
+
+src/s_math.o: src/s_math.s
+	$(AS) -o src/s_math.o src/s_math.s
 
 test: all
 	$(CC) $(CFLAGS) -o test/main test/main.c -Llib -ls_libc
--- test/main.c
+++ test/main.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
-#include "s_string.h"
 
 extern int s_strlen(const char *str);
+extern char* s_strcpy(char *dest, const char *src);
+extern int s_strncmp(const char *str1, const char *str2, int n);
+extern int s_abs(int x);
+extern int s_pow(int base, int exp);
 
 int main() {
-    char *test = "Hello, world!";
-    printf("Longueur : %d\n", s_strlen(test));
+    char source[] = "Hello, World!";
+    char dest[50];
+
+    // Tests pour s_string.s
+    printf("Longueur de '%s' : %d\n", source, s_strlen(source));
+    s_strcpy(dest, source);
+    printf("Copie de '%s' : '%s'\n", source, dest);
+    if (s_strncmp(source, dest, 5) == 0)
+        printf("Les 5 premiers caractères sont égaux.\n");
+    else
+        printf("Les 5 premiers caractères sont différents.\n");
+
+    // Tests pour s_math.s
+    printf("Valeur absolue de -42 : %d\n", s_abs(-42));
+    printf("2 puissance 4 : %d\n", s_pow(2, 4));
+
     return 0;
 }
+
